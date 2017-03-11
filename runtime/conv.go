f5a441f39facded5517116540eb17291a03abe79
Mitchell Cooper <githubspam@rlygd.net>
2017-03-10 22:53:55 -0500

    allow any value in Fstring

--- runtime/conv.go
+++ runtime/conv.go
@@ -1,5 +1,19 @@
 package runtime
 
-func Fstring(s string) *String {
+import "fmt"
+
+func Fstring(i interface{}) *String {
+	s := ""
+	switch v := i.(type) {
+	case *String:
+		return v
+	case string:
+		s = v
+	case nil:
+		s = "(undefined)"
+		// TODO: case Object, check for toString
+	default:
+		s = fmt.Sprintf("%v", v)
+	}
 	return &String{s, objectBase()}
 }
