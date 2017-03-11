579a5e33424d782178e4cdb29852c6b679331d3e
Mitchell Cooper <githubspam@rlygd.net>
2017-03-11 18:08:25 -0500

    rly bad numbers

---
+++ runtime/number.go
@@ -0,0 +1,21 @@
+package runtime
+
+// import "reflect"
+import "fmt"
+
+type Number struct {
+	IntValue   int64
+	FloatValue float64
+	*genericObject
+}
+
+func (n *Number) AsFloat() float64 {
+	if n.FloatValue != 0 {
+		return n.FloatValue
+	}
+	return float64(n.IntValue)
+}
+
+func (n *Number) Description(d *DescriptionOption) string {
+	return fmt.Sprintf("%v", n.AsFloat())
+}
